64c06946e1ffa5f892544615f585499619fe3fd1
IndraHardika <92752408+KZdra@users.noreply.github.com>
2023-09-11 08:06:52 +0700

    done stengah

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let counter = 1
 setInterval(()=>{
 
     document.querySelector('.img.show').classList.remove('show')
-    const img = document.querySelector(".img-${counter}")
+    const img = document.querySelector(`.img-${counter}`)
     img.classList.add('show')
     counter++
 
